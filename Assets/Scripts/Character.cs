048d2a1cdc0f3c6a23e89c45d67e3b7ade3376cc
kiasma99 <josangho0905@gmail.com>
2023-03-11 21:22:45 +0900

    tile & animation

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+    private Animator animator;
     public bool PressStart = false;
     Vector3 moveVelocity = Vector3.zero;
     float moveSpeed = 4;
@@ -11,12 +12,33 @@ public class Character : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         transform.position = new Vector3(0, -1, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetInteger("WalkType", 1);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            animator.SetInteger("WalkType", 2);
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            animator.SetInteger("WalkType", 2);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            animator.SetInteger("WalkType", 3);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
         if (PressStart)
         {
             moveVelocity = new Vector3(0, 0.10f, 0);
